8a54b89cd410064eed9fc763fbe56a404bc6ac73
Chet25 <chetan25december@gmail.com>
2023-11-14 10:06:36 +0530

    details page update

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -2,6 +2,21 @@ import Header from "../components/Header";
 import OutputField from "../components/OutputField";
 import person from "../assets/person.jpeg"
 
+
+
+// webpack compiled with 1 warning
+// One of your dependencies, babel - preset - react - app, is importing the
+// "@babel/plugin-proposal-private-property-in-object" package without
+// declaring it in its dependencies.This is currently working because
+// "@babel/plugin-proposal-private-property-in-object" is already in your
+// node_modules folder for unrelated reasons, but it may break at any time.
+
+//     babel - preset - react - app is part of the create - react - app project, which
+// is not maintianed anymore.It is thus unlikely that this bug will
+// ever be fixed.Add "@babel/plugin-proposal-private-property-in-object" to
+// your devDependencies to work around this error.This will make this message
+// go away.
+
 export default function Details() {
     const styles = {
         profile_image: {
@@ -31,7 +46,7 @@ export default function Details() {
     }
 
     return (
-        <div style={{ padding: "1em", overflowY: "scroll" }}>
+        <div style={{ padding: "1em 1em 3em 1em" }}>
             <Header text={"Details"} />
 
             <div style={{ display: "flex", alignItems: "flex-end", gap: "0.5em", marginBottom: "2em" }}>
