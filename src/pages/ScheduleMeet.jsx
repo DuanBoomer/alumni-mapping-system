29d5c0d5df6ffb831d277fdd8eed92b94dcb3756
Chet25 <chetan25december@gmail.com>
2023-11-16 10:14:56 +0530

    schedule page

--- src/pages/ScheduleMeet.jsx
+++ src/pages/ScheduleMeet.jsx
@@ -4,13 +4,12 @@ import { useState } from "react";
 
 export default function ScheduleMeet() {
 
-    const [time, setTime] = useState("")
+    const [startTime, setStartTime] = useState("")
+    const [endTime, setEndTime] = useState("")
     const [title, setTitle] = useState("")
     const [desc, setDesc] = useState("")
     const [date, setDate] = useState("")
     const [link, setLink] = useState("")
-
-    //  onChange={(event) => setAlumniData({ ...alumniData, desc: event.target.value })} , value={alumniData.desc}
 
     return (
         <div style={{ padding: "1em 1em 3em 1em" }}>
@@ -18,8 +17,9 @@ export default function ScheduleMeet() {
             <Header text={"Schedule Meet"} />
             <InputField title={"Title"} placeholder={"enter a title"} type={"text"} state={title} setState={setTitle} />
             <InputField title={"Description"} placeholder={"enter a desc"} type={"textarea"} state={desc} setState={setDesc} />
-            <InputField title={"Time"} placeholder={"time"} type={"time"} state={time} setState={setTime} />
-            <InputField title={"Date"} placeholder={"date"} type={"date"} state={date} setState={setDate} />
+            <InputField title={"Start Time"} type={"time"} state={startTime} setState={setStartTime} />
+            <InputField title={"End Time"} type={"time"} state={endTime} setState={setEndTime} />
+            <InputField title={"Date"} type={"date"} state={date} setState={setDate} />
             <InputField title={"Link"} placeholder={"meet link"} type={"url"} state={link} setState={setLink} />
 
             <div style={{ height: "calc(0.5em + 26px)" }}></div>
